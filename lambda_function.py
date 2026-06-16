0e32316360921dadf8a2afb15ac935e81b5ddfc8
Rohan Dubasi <dubasirohan@gmail.com>
2026-06-16 11:58:00 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "CI/CD working "})
+        "body": json.dumps({"message": "CI/CD connection working "})
     }
